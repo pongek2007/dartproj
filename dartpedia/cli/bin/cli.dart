760ff966784d1bf66e9fbbddb92a8990b6cd39d9
pongek2007 <pongek2007@gmail.com>
2026-05-27 12:27:28 +0700

    แก้ไขเพิ่มเติมต่อ

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,12 +1,36 @@
-//import 'dart:io';
-import 'package:http/http.dart' as http;
+/*import 'package:command_runner/command_runner.dart';
+
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+  commandRunner.run(arguments);
+}*/
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) async { // main is now async and awaits the runner
-
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>
+void main(List<String> arguments) {
+  // [Step 6 update] Add onError method
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
 
